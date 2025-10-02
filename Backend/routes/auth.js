2d118eae98a3f4c194d08b376a8b6621932536d8
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-10-02 08:17:33 +0545

    push

---
+++ Backend/routes/auth.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+const router = express.Router();
+const JWT_SECRET = process.env.JWT_SECRET;
+
+//register
+router.post("/register", async (req, res) => {
+    try {
+        const {name, email, password, role} = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({name, email, password:hashedPassword, role});
+        await user.save();
+        res.json({message: "user register successfully"});
+        
+    } catch (error) {
+        res.status(400).json({error: error.message});
+        
+    }
+});
+
+
+
+//login
+router.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+    const user= await User.findOne({email});
+    if(!user) return res.status(400).json({error: "user not found"});
+
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch)  return res.status(400).json({error: "Invalid credentials"})
+
+    const token = jwt.sign({id: user._id}, JWT_SECRET, {expiresIn: '1h'});
+    res.json({token});
+});
+
+export default router;
